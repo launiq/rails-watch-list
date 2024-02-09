4d854502068355f3575ed108cc83ad06fe8e58d1
LauraNicolas <laura.cecilia.nicolas@gmail.com>
2024-02-09 18:31:03 +0100

    created watch-list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,20 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmarks = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+end
